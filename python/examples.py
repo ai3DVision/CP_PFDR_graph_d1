d50686e2d33dab520bdb81b20c3b91a353ebe87e
Loic Landrieu <landrieu.loic@gmail.com>
2018-02-19 15:15:34 +0100

    update python wrapper

--- python/examples.py
+++ python/examples.py
@@ -5,28 +5,28 @@ import libCP
 #
 #  V1 -- V2 -- V3
 #
-# F(x) = 1/2 * (4 *(x1-10)^2 + 4 * (x2)^2 + 4 * (x3-10)^2)
+# F(x) = 1/2 * ((x1-10)^2 + (x2)^2 + (x3-10)^2)
 #      + 3 * |x1-x2| + 3 * |x2-x3| 
 #      + 0.5* (|x1| + |x2| + |x3|)
-source = np.array([0, 1], dtype = 'uint32')
-target = np.array([1, 2], dtype = 'uint32')
-obs = np.array([10.,0.,10.], dtype = 'float32')
-edge_weight = 3*np.array([1., 1.], dtype = 'float32')
-A = np.array([[2, 0., 0,], [0., 2., 0,], [0., 0., 2,]], dtype = 'float32')
-l1_weight = 0.5 * np.array([1., 1., 1.], dtype = 'float32')
+data_type = 'f4'
+source = np.array([0, 1], dtype='uint32')
+target = np.array([1, 2], dtype='uint32')
+obs = np.array([10.,0.,10.], dtype=data_type)
+edge_weight = 3*np.array([1., 1.], dtype=data_type)
+A = np.array([[1, 0., 0,], [0., 1., 0,], [0., 0., 1,]],dtype =data_type)
+l1_weight = 0.5 * np.array([1., 1., 1.], dtype=data_type)
 positivity = 0
-speed = 2 #fast
 
 #all the following calls give the same results:
-cV, rX = libCP.CP_PFDR_graph(obs, source, target, edge_weight, A, l1_weight, positivity, speed)
+cV, rX = libCP.CP_quadratic_l1(obs, source, target, edge_weight, A, l1_weight)
 print(rX[cV])
-cV, rX = libCP.CP_PFDR_graph(obs, source, target, edge_weight, A, 0.5, positivity, speed)
+cV, rX = libCP.CP_quadratic_l1(obs, source, target, edge_weight, A, 0.5)
 print(rX[cV])
-cV, rX = libCP.CP_PFDR_graph(obs, source, target, 3, A, 0.5, positivity, speed)
+cV, rX = libCP.CP_quadratic_l1(obs, source, target, 3., A, 0.5)
 print(rX[cV])
-cV, rX = libCP.CP_PFDR_graph(obs, source, target, 3, np.array([2, 2., 2,], dtype = 'float32'), positivity, speed)
+cV, rX = libCP.CP_quadratic_l1(obs, source, target, 3., np.array([1., 1., 1.,], dtype =data_type), 0.5)
 print(rX[cV])
-cV, rX = libCP.CP_PFDR_graph(obs, source, target, 3, 2, positivity, speed)
+cV, rX = libCP.CP_quadratic_l1(obs, source, target, 3., 1., 0.5)
 print(rX[cV])
 
 #EEG example
@@ -34,7 +34,6 @@ print(rX[cV])
 #Graph Total Variation`` for details
 #Courtesy of Ahmad Karfoul et Isabelle Merlet, LTSI,INSERM U1099.
 import scipy.io
-from timeit import default_timer as timer
 eeg = scipy.io.loadmat('data_CP_EEG.mat')
 
 obs = np.array(eeg["y"][:,0], dtype='f8')
@@ -44,12 +43,4 @@ edge_weight = np.array(eeg["La_d1"][:,0], dtype='f8')
 A = np.array(eeg["Phi"], dtype='f8')
 l1_weight = np.array(eeg["La_l1"][:,0], dtype='f8')
 
-start = timer()
-cV, rX = libCP.CP_PFDR_graph(obs, source, target, edge_weight, A, l1_weight, 1, 0)
-print("slow setting = %f" % (timer()-start))
-start = timer()
-cV, rX = libCP.CP_PFDR_graph(obs, source, target, edge_weight, A, l1_weight, 1, 1)
-print("standard setting = %f" % (timer()-start))
-start = timer()
-cV, rX = libCP.CP_PFDR_graph(obs, source, target, edge_weight, A, l1_weight, 1, 2)
-print("fast setting = %f" % (timer()-start))
+cV, rX = libCP.CP_quadratic_l1(obs, source, target, edge_weight, A, l1_weight, positivity=1)
